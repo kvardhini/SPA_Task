9ffe4c360f776b6cf7308f3cec3c424072c56e64
Vardhini.Karangi <kvardhini10@gmail.com>
2025-06-25 18:00:09 +0530

    Home page is created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,13 +20,13 @@ function HomePage(){
                     </div>
                 </div>
                  <div className="card" style={{width: "18rem"}}>
-                    <img src="./All_images/cooking.webp" className="card-img-top" alt="Programming-image" /> 
+                    <img src="./All_images/cooking.webp" className="card-img-top" alt="Cooking-image" /> 
                     <div className="card-body">
                         <h5 className="card-title">Cooking </h5>   
                     </div>
                 </div>
                  <div className="card" style={{width: "18rem"}}>
-                    <img src="./All_images/workout.jpg" className="card-img-top" alt="Programming-image" /> 
+                    <img src="./All_images/workout.jpg" className="card-img-top" alt="Workout-image" /> 
                     <div className="card-body">
                         <h5 className="card-title">Workout </h5>   
                     </div>
